3b39d9a095893cca253542ef32ef06210471cd17
priscilla martinez <priscillamp1207@gmail.com>
2026-06-03 20:12:33 -0600

    leccion3

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,1 +1,28 @@
-Vista Superior
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/main.css">
+        <title>Mis viajes</title>
+    </head>
+    <body>
+      <header>
+            <menu>
+                <div class="menu izquierda">
+                   <a href=./index.php>
+                      <img src="./img/logo.png" alt="logo">
+                   </a>
+
+                </div>
+
+                <div class="menu izquierda">
+                 <a href="./index.php">Inicio</a>
+                 <a href="./boletos.php">boletos</a>
+                  <a href="./precio.php">precios</a>
+                   <a href="./avion.php">aviones</a>
+               </div>
+
+             </menu>
+      </header>
+    
